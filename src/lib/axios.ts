d3143ec5336753d5e1b1cdab0bb8718c3722d497
Otabek Narziqulov <otabeknarz@gmail.com>
2025-02-16 19:30:50 +0500

    fixed csrf token url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -15,8 +15,20 @@ instance.interceptors.request.use(async (config) => {
   // Only get CSRF token for non-GET requests
   if (config.method !== 'get') {
     try {
-      const response = await axios.get(API_URLS.GET_CSRF_TOKEN);
-      config.headers['X-CSRFToken'] = response.data.csrfToken;
+      // Get CSRF token from cookie
+      const csrfToken = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('csrftoken='))
+        ?.split('=')[1];
+
+      if (csrfToken) {
+        // Set the token in the header
+        config.headers['X-CSRFToken'] = csrfToken;
+      } else {
+        // If no token in cookie, get a new one
+        const response = await axios.get(API_URLS.GET_CSRF_TOKEN);
+        config.headers['X-CSRFToken'] = response.data.csrfToken;
+      }
     } catch (error) {
       console.error('Failed to get CSRF token:', error);
     }
