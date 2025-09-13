c944bc8500628a42f9b9e3dd06f616069fc8f84f
Otabek Narz <otabeknarz@gmail.com>
2025-09-13 05:32:41 +0500

    fully production

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ["api.smart-fit.uz"],
-  },
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "3df15cd5507532f9026af02a9599fc77.r2.cloudflarestorage.com",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
